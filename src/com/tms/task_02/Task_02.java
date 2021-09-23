a36f509d28bfa068ddcd2cae0fb7ea171e130d7f
Piligrihm <71782601+Piligrihm@users.noreply.github.com>
2021-09-24 00:31:33 +0300

    Add files via upload

--- src/com/tms/task_02/Task_02.java
+++ src/com/tms/task_02/Task_02.java
@@ -2,17 +2,10 @@ package com.tms.task_02;
 
 import com.tms.task_02.tester.DocNumTester;
 
-/**
- * TODO d:\\TeachMeSkills\\TMS_Lesson_10_HW\\1.txt
- * TODO
- * TODO Разбить код на отдельные методы
- * TODO пройтись по HashMap и из нее записать инфу в файл отчет
- */
-
 public class Task_02 {
     public static void main(String[] args) {
         DocNumTester docNumWorker = new DocNumTester();
-        docNumWorker.startGame();
+        docNumWorker.startProg();
 
     }
 }
